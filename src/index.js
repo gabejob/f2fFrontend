522624b53c3866052b562a3f7d0d212337beccef
gabee <u1028866@umail.utah.edu>
2022-05-19 12:23:58 -0600

    s

--- src/index.js
+++ src/index.js
@@ -3,18 +3,11 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.css";
 const container = document.getElementById("root");
-const script = document.createElement('script');
-script.src="https://platform.twitter.com/widgets.js";
-script.async=true;
-document.body.appendChild(script);
-
-
 
 const root = createRoot(container);
 
 root.render(
-
-        <div  className="baseplate">
-            <App />
-        </div >
+  <div className="baseplate">
+    <App />
+  </div>
 );
